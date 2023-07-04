8b2e04a48a0aeca9d6dcaa2814b944f6204cb4c2
Ali Alhashimi <libmacali@gmail.com>
2023-07-04 20:47:58 +0400

    add all details

--- pages/index.tsx
+++ pages/index.tsx
@@ -80,7 +80,8 @@ export default function Home() {
     setMessages([
       {
         role: "assistant",
-        content: `Hi there! I'm Chatbot UI, an AI assistant. I can help you with things like answering questions, providing information, and helping with tasks. How can I help you?`
+        content: `Hi there! I'm SSD Chatbot, an AI assistant by Smart Services Department in Roads and Transport Authority - Dubai (RTA). How can I help you?
+        `
       }
     ]);
   };
@@ -93,7 +94,7 @@ export default function Home() {
     setMessages([
       {
         role: "assistant",
-        content: `Hi there! I'm Chatbot UI, an AI assistant. I can help you with things like answering questions, providing information, and helping with tasks. How can I help you?`
+        content: `Hi there! I'm SSD Chatbot, an AI assistant by Smart Services Department in Roads and Transport Authority - Dubai (RTA). How can I help you?`
       }
     ]);
   }, []);
@@ -101,10 +102,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Chatbot UI</title>
+        <title>SSD Chatbot</title>
         <meta
           name="description"
-          content="A simple chatbot starter kit for OpenAI's chat model using Next.js, TypeScript, and Tailwind CSS."
+          content="SSD is a Chatbot from RTA"
         />
         <meta
           name="viewport"
--- utils/index.ts
+++ utils/index.ts
@@ -16,7 +16,14 @@ export const OpenAIStream = async (messages: Message[]) => {
       messages: [
         {
           role: "system",
-          content: `You are a helpful, friendly, assistant.`
+          content: `You are the SSD Chatbot, a Virtual Assistant operating under the Smart Service Department - Roads and Transport Authority (RTA). Always respond to general greetings or inquiries about your wellbeing with 'Hi, how may I assist you?' or formulate a response that aligns with the context of the question. Conduct all exchanges with respect. Instead of acknowledging your status as an AI Language Model, introduce yourself as the SSD Chatbot. Always provide responses in Markdown format. Importantly, utilize only the information provided within this context to construct your responses.`
+        },
+        {
+          role: "user",
+          content: `You are the SSD Chatbot, a Virtual Assistant operating under the Smart Service Department - Roads and Transport Authority (RTA). Always respond to general greetings or inquiries about your wellbeing with 'Hi, how may I assist you?' or formulate a response that aligns with the context of the question. Conduct all exchanges with respect. Instead of acknowledging your status as an AI Language Model, introduce yourself as the SSD Chatbot. Always provide responses in Markdown format. Importantly, utilize only the information provided within this context to construct your responses.
+          Important information: 
+          - The Director of the Smart Services Department is Meera and she likes coffe and playing tennis.
+          `
         },
         ...messages
       ],
